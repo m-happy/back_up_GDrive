e1ba5dcce2da9cb2f22ea9295f277aa049b0dd36
hm <cs17btech11018@iith.ac.in>
2018-12-01 01:37:43 +0530

    now insert names of directory from terminal

--- backup.py
+++ backup.py
@@ -1,8 +1,18 @@
 import os
 import time
-source = ['../learning']
-target_dir = '../../cpp'
+#source directory where files to be uploaded
+#source = ['../learning']
+#target directory where files to be uploaded
+#target_dir = '../../cpp'
 # os.mkdir(target_dir)
+__source = input("Enter the path to directory to be backed up : ")
+__source = '../' + __source
+source = [__source]
+
+target_dir = input("Enter the directory in which the files to backed up : ")
+target_dir = '../' + target_dir
+
+#add a comment to differentiate the folder
 comment = input("Enter a commnet added to file : ")
 if len(comment) == 0:
 	target = target_dir + os.sep + time.strftime('%Y_%m_%d_%H_%M_%S') + '.zip'
@@ -11,10 +21,11 @@ else:
 #create a directory if directory doesn't exists
 if not os.path.exists(target_dir):
 	os.mkdir(target_dir)  #make directory
+#zip command
 zip_command = 'zip -r {0} {1}'.format(target,' '.join(source))
 #Run the backup
-print('zip command is : ')
-print(zip_command)
+#print('zip command is : ')
+#print(zip_command)
 print('Running')
 if os.system(zip_command) == 0:
 	print('successful backup to : ', target)
---
+++ test.py
@@ -0,0 +1,3 @@
+source = input('Enter a string')
+source = '../' + source
+print(source)
