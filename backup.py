5ef9f023bc653584b77396255750ba80990d8041
hm <cs17btech11018@iith.ac.in>
2018-12-01 01:42:27 +0530

    clear code

--- backup.py
+++ backup.py
@@ -1,14 +1,10 @@
 import os
 import time
-#source directory where files to be uploaded
-#source = ['../learning']
-#target directory where files to be uploaded
-#target_dir = '../../cpp'
-# os.mkdir(target_dir)
+#input for the source file to be backed
 __source = input("Enter the path to directory to be backed up : ")
 __source = '../' + __source
 source = [__source]
-
+#target for the source file where source file should be backed
 target_dir = input("Enter the directory in which the files to backed up : ")
 target_dir = '../' + target_dir
 
@@ -24,8 +20,7 @@ if not os.path.exists(target_dir):
 #zip command
 zip_command = 'zip -r {0} {1}'.format(target,' '.join(source))
 #Run the backup
-#print('zip command is : ')
-#print(zip_command)
+
 print('Running')
 if os.system(zip_command) == 0:
 	print('successful backup to : ', target)
